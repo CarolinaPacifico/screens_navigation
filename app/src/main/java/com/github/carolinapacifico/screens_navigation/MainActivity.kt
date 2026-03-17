135cd722c29010a93bc84b893ed31f0440c62882
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-17 19:50:56 -0300

    Remove exemplos e limpa a MainActivity para futura implementação

---
+++ app/src/main/java/com/github/carolinapacifico/screens_navigation/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.github.carolinapacifico.screens_navigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.github.carolinapacifico.screens_navigation.ui.theme.Screens_navigationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Screens_navigationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
